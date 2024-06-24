7f6b567fdd733320e2a5dacaf1f9c77ad2804776
brachiotusk <127916417+brachiotusk@users.noreply.github.com>
2024-06-23 18:45:08 -0700

    Create hqa.py
    first class for high quality asset, use SE-CCR to determine credit risk

---
+++ hqa.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import pandas as pd
+import math
+import scikit as sk
+import matplotlib as mp
+
+class HQLA(object):
+    #based off credit risk over a certain maturity following https://www.bis.org/publ/bcbs279.pdf
+    exchanges = ["XNYS", "XNAS", "XAMS", "XBRU", "XMSM", "XLIS", "XMIL", "XOSL", "XPAR", "XSHG", "XJPX", "XSHE", "XBOM", "XNSE", "XHKG", "XTSE", "XLON", "XSAU", "XFRA", "XSWX", "XCSE", "XSTO", "XHEL", "XTAL", "XRIS", "XLIT", "XICE"]
+    def risk(credit, tenor):
+    def valuation():
+    def correlation():
+    def transparency(Exc):
+        for x in exchanges:
+            if x == Exc:
+                print("Transparent")
+    def market():
+    def volatility():
+    
